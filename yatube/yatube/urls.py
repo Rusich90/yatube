535fc0b9064c6e1cbf97af7a725c7d4cee0e3d91
Руслан Алимов <rusichviz@gmail.com>
2021-03-07 19:51:44 +0300

    Add registration form for users

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -18,5 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("", include("posts.urls")),
+    path("auth/", include("users.urls")),
+    path("auth/", include("django.contrib.auth.urls")),
     path('admin/', admin.site.urls),
 ]
