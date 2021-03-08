f403e58431ed9f825ea28dc1ec05a76015656b99
Руслан Алимов <rusichviz@gmail.com>
2021-03-08 22:09:08 +0300

    i don't know what i done

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -132,3 +132,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 LOGIN_URL = "/auth/login/"
 LOGIN_REDIRECT_URL = "index" 
 # LOGOUT_REDIRECT_URL = "index"
+
+#  подключаем движок filebased.EmailBackend
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+# указываем директорию, в которую будут складываться файлы писем
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
