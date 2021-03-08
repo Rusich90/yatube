2684c921abb9ff76a96d56681d7dc0abd95326b9
Руслан Алимов <rusichviz@gmail.com>
2021-03-08 15:15:00 +0300

    Added login, logout, password_change, password_reset

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -125,3 +125,10 @@ USE_TZ = False
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+
+# Login
+
+LOGIN_URL = "/auth/login/"
+LOGIN_REDIRECT_URL = "index" 
+# LOGOUT_REDIRECT_URL = "index"
