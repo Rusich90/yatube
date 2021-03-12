4a0010aafa6e3be929b6c5bb98b1d84ec4a45394
Rusich <RusichViz@gmail.com>
2021-03-12 12:02:28 +0300

    Now new_post realy work

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
-from .models import Post, Group
+from .models import Post, Group, User
 from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     latest = Post.objects.order_by('-pub_date')[:11]
@@ -15,14 +16,15 @@ def group_posts(request, slug):
     
     return render(request, "group.html", {"group": group, "posts": posts})
 
-
+@login_required
 def new_post(request):
-    if requst.method == "POST":
-        form = PostForm(request.POST):
+    if request.method == "POST":
+        form = PostForm(request.POST)
         if form.is_valid():
             text = form.cleaned_data['text']
-            group = form.cleaned_data['group']
-            return redirect ('/index/')
+            group = form.cleaned_data['group'] 
+            Post.objects.create(author=request.user , group=group, text=text)
+            return redirect ('index')
 
         return render(request, 'new_post.html', {"form": form})
 
