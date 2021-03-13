4832be5458e2aa43be74c2b5d0fe7c83e5421315
Rusich <RusichViz@gmail.com>
2021-03-13 18:22:16 +0300

    Adde paginator

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,18 +3,25 @@ from django.http import HttpResponse
 from .models import Post, Group, User
 from .forms import PostForm
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 def index(request):
-    latest = Post.objects.order_by('-pub_date')[:11]
+    post_list = Post.objects.order_by('-pub_date').all()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
     
-    return render(request, 'index.html', {'posts': latest})
+    return render(request, 'index.html', {'page': page, 'paginator': paginator})
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.all()[:12]
+    group_post_list = group.posts.all()
+    paginator = Paginator(group_post_list, 5)
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)    
     
-    return render(request, "group.html", {"group": group, "posts": posts})
+    return render(request, "group.html", {"group": group, 'page': page, 'paginator': paginator})
 
 @login_required
 def new_post(request):
