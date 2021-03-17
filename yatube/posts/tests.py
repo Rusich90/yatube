5e3bf3d7a32ee452f86c043dd9f83794e1e66018
Rusich <RusichViz@gmail.com>
2021-03-17 13:37:34 +0300

    Added new test

--- yatube/posts/tests.py
+++ yatube/posts/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase, Client
 from .models import Post, User
 
+
 class ProfileTest(TestCase):
     def setUp(self):
         self.client = Client()
@@ -13,3 +14,28 @@ class ProfileTest(TestCase):
         self.assertEqual(len(response.context["page"]), 1)
         self.assertIsInstance(response.context["profile"], User)
         self.assertEqual(response.context["profile"].username, self.user.username)
+
+
+class NewPostTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='test_user', email='test@test.com', password='12345678')
+        self.post = Post.objects.create(text='test text', author=self.user)
+
+    def test_newpost_notlogged(self):
+        response = self.client.get('/new/')
+        self.assertEqual(response.status_code, 302)
+        self.assertTemplateUsed(response, 'login.html')
+
+    def test_newpost_loggeduser(self):
+        self.client.login(username='test_user', password='12345678')
+        response = self.client.get('/new/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_new_post(self):
+        self.post = Post.objects.create(text='test text', author=self.user)
+        response = self.client.get('/index/')
+        self.assertEqual(response.context['paginator'][0].text, 'test text')
+
+
+
