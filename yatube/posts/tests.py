9cada97f5d0b38aa4e086216d501c54578990214
Руслан Алимов <rusichviz@gmail.com>
2021-03-17 22:22:10 +0300

    Change tests

--- yatube/posts/tests.py
+++ yatube/posts/tests.py
@@ -1,47 +1,49 @@
 from django.test import TestCase, Client
 from .models import Post, User
+from django.urls import reverse
+
 
 
 class ProfileTest(TestCase):
     def setUp(self):
-        self.client = Client()
-        self.user = User.objects.create_user(username='test_user', email='test@test.com', password='12345678')
-        self.post = Post.objects.create(text='test text', author=self.user)
+        self.not_auth_client = Client()
+        self.auth_client = Client()
+        self.user = User.objects.create_user(
+            username='test_user',
+            email='test@test.com',
+            password='12345678'
+        )
+        self.auth_client.login(username='test_user', password='12345678')
+
+        #self.post = Post.objects.create(text='test text', author=self.user)
 
     def test_profile(self):
-        response = self.client.get('/test_user/')
+        response = self.auth_client.get(reverse('profile', kwargs={'username': 'test_user'}))
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.context["page"]), 1)
         self.assertIsInstance(response.context["profile"], User)
         self.assertEqual(response.context["profile"].username, self.user.username)
 
-
-class NewPostTest(TestCase):
-    def setUp(self):
-        self.client = Client()
-        self.user = User.objects.create_user(username='test_user', email='test@test.com', password='12345678')
-        self.post = Post.objects.create(text='test text', author=self.user)
-
     def test_newpost_notlogged(self):
-        response = self.client.get('/new/')
-        self.assertEqual(response.status_code, 302)
-        #self.assertTemplateUsed(response, 'login.html')
+        self.not_auth_client.post(reverse('new_post'), data={'text': 'test text'})
+        response = self.not_auth_client.get(reverse('profile', kwargs={'username': 'test_user'}))
+        self.assertEqual(len(response.context["page"]), 0)
 
     def test_newpost_loggeduser(self):
-        self.client.login(username='test_user', password='12345678')
-        response = self.client.get('/new/')
-        self.assertEqual(response.status_code, 200)
+        self.auth_client.post(reverse('new_post'), data={'text': 'test text'})
+        response = self.auth_client.get(reverse('profile', kwargs={'username': 'test_user'}))
+        self.assertEqual(len(response.context["page"]), 1)
+        self.assertEqual( response.context['page'][0].text, 'test text')
 
-    def test_new_post(self):
-        self.post = Post.objects.create(text='test text', author=self.user)
+    def test_edit_post(self):
+        self.auth_client.post(reverse('new_post'), data={'text': 'test text'})
+        self.auth_client.post(reverse('post_edit',
+                                      kwargs={
+                                          'username': self.user.username,
+                                          'post_id': 1}),
+                                      data={'text': 'edit test text'})
         response_index = self.client.get('/')
         response_profile = self.client.get('/test_user/')
         response_post = self.client.get('/test_user/1/')
-        self.assertEqual(response_index.context['page'][0].text, 'test text')
-        self.assertEqual(response_profile.context['page'][0].text, 'test text')
-        self.assertEqual(response_post.context['author_post'].text, 'test text')
-        self.post.text = 'edit test text'
-        self.post.save()
         self.assertEqual(response_index.context['page'][0].text, 'edit test text')
         self.assertEqual(response_profile.context['page'][0].text, 'edit test text')
         self.assertEqual(response_post.context['author_post'].text, 'edit test text')
