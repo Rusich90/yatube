47a9f1d0db9a8e8c9683d1774cb4436afaf3ea8c
Rusich <RusichViz@gmail.com>
2021-03-18 15:44:19 +0300

    Added help_text and verbose_name in Models

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -15,10 +15,15 @@ class Group(models.Model):
 
 
 class Post(models.Model):
-    text = models.TextField()
+    text = models.TextField(verbose_name='Текст', help_text='Введите текст')
     pub_date = models.DateTimeField("date published", auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
-    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name="posts", blank=True, null=True)
+    group = models.ForeignKey(Group,
+                              on_delete=models.CASCADE,
+                              related_name="posts",
+                              verbose_name='Группа',
+                              help_text='Выберите группу для поста (необязательно)',
+                              blank=True, null=True)
 
     def __str__(self):
         # выводим текст поста
